4cee898ddfbe6ddfec277847c6e34301c42b8eb4
Max Base <MaxBaseCode@Gmail.Com>
2019-05-26 22:26:27 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : RookPolynomial/script.js
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-05-26
+* @Released under : https://github.com/BaseMax/RookPolynomial/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/RookPolynomial
+*
+**/
